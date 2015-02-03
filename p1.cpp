4f620b62ee381e49ff91a6e9b9665f602d796a14
Vivek Agarwal <vivek17.agarwal@gmail.com>
2015-02-03 15:51:34 +0530

    new file

---
+++ p1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int max(int a,int b){ return a>b?a:b;}
+int dp[1004][1003][3];
+int ar[][2]={{3,2},{-5,-10},{-20,5}};
+int call(int h,int a,int cur)
+{
+    if(h<=0||a<=0) return 0;
+    if(dp[h][a][cur]!=0) return dp[h][a][cur];
+    int t=-1;
+    for(int i=0;i<3;i++)
+    {
+        if(i!=cur)
+            t=max(t,1+call(h+ar[i][0],a+ar[i][1],i));
+            //cout<<"t= "<<t<<endl;
+
+    }
+    return (dp[h][a][cur]=t);
+}
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+    {
+        int h,a;
+        cin>>h>>a;
+       // cout<<ar[0][0]<<" "<<ar[0][1]<<endl;
+        cout<<call(h+ar[0][0],a+ar[0][1],0)<<endl;
+    }
+	return 0;
+}
